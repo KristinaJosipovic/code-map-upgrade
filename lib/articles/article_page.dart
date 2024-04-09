d583db4988bde4ad27e92f509dbde39ce1fdf7b0
Emir Halilović <66942813+EmirYNWA@users.noreply.github.com>
2024-04-09 19:17:51 +0200

    Created article stateful widget and added one more category.

---
+++ lib/articles/article_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainArticle extends StatefulWidget {
+  const MainArticle({super.key});
+
+  @override
+  State<MainArticle> createState() => _MainArticleState();
+}
+
+class _MainArticleState extends State<MainArticle> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
