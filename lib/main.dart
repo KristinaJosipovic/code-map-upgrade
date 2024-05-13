608678daa04a60c1c53d5a87d4b6319347297537
Emir Halilović <66942813+EmirYNWA@users.noreply.github.com>
2024-05-13 19:23:48 +0200

    Altered project structure a bit, deleted unused files.

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'service/firebase_options.dart';
+import 'firebase_options.dart';
 import 'package:code_map/screens/welcome_screen.dart';
 
 void main() async{
